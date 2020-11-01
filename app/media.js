5b790f01c2557688b4dfe41101323703ef77f260
Łukasz Borys <lukasz_300@wp.pl>
2020-11-01 07:49:54 +0100

    Add files via upload

--- app/media.js
+++ app/media.js
@@ -1,11 +1,12 @@
 export class Media {
     constructor(){
-       this.backgroundSound = new Audio('../sounds/background.mp3');
-       this.cardShuffle = new Audio('../sounds/cardshuffle.wav');
-       this.cardSlide = new Audio('../sounds/card_slide.wav')
+       this.backgroundSound = new Audio();
+       this.cardShuffle = new Audio();
+       this.cardSlide = new Audio()
     }
 
     playMusic(){
+        this.backgroundSound.src = ('../sounds/background.mp3');
         this.backgroundSound.loop = true;
         this.backgroundSound.volume = 1;
         this.backgroundSound.autoplay = false;
@@ -13,12 +14,14 @@ export class Media {
         }
 
     playCardShuffle(){
+        this.cardShuffle.src = ('../sounds/cardshuffle.wav');
         this.cardShuffle.loop = false;
         this.cardShuffle.volume = 1;
         this.cardShuffle.autoplay = false;
         this.cardShuffle.play();
     }
     playCardSlide(){
+        this.cardSlide.src = ('../sounds/card_slide.wav');
         this.cardSlide.loop = false;
         this.cardSlide.volume = 1;
         this.cardSlide.autoplay = false;
